8700ca979e142bc6a971fdbedcdb4f94b215ccea
Lasana Murray <lmurray@quenk.com>
2016-07-05 00:10:22 -0400

    Check in new parse tests.

--- test/parse.js
+++ test/parse.js
@@ -41,7 +41,138 @@ tests = {
     'should parse preamble': [{
         input: `import lib from "path/to/libs"; <tag/>`,
         expect: expects['should parse preamble'][0]
-    }]
+    }, {
+        input: `import * as lib from "path/to/libs"; <tag/>`,
+        expect: expects['should parse preamble'][1]
+    }],
+    'should parse a self closing tag': {
+        input: '<simple/>',
+        expect: expects['should parse a self closing tag']
+    },
+    'should parse a self closing tag with attributes': [{
+        input: '<user name="xyaa aaz" position={{4|x(20)}} wml:val="test"/>',
+        expect: expects['should parse a self closing tag wxith attributes'][0]
+
+    }, {
+        input: '<user enabled id=24 />',
+        expect: expects['should parse a self closing tag wxith attributes'][1]
+    }, {
+        input: '<user name="xyaa aaz" id="24" align="left"/>',
+        expect: expects['should parse a self closing tag wxith attributes'][2]
+
+    }],
+
+    'should parse a parent tag': {
+        input: '<panel>  \n\n\n\n\n\n\n\n\n  </panel>',
+        expect: expects['should parse a parent tag']
+    },
+    'should parse a parent tag with attributes': {
+        input: '<panel type="default" size="40" align="left"> </panel>',
+        expect: expects['should parse a parent tag with attributes']
+
+    },
+    'should parse parent tags wxith mixed children': {
+
+        input: '<panel> This is my offsprings.<a>Link</a>Hey now! <Input/></panel>',
+        expect: expects['should parse parent tags with mixed children']
+
+    },
+
+    'should parse parent tags with tag children (L1)': {
+
+        input: '<panel><a></a></panel>',
+        expect: expects['should parse parent tags with tag children (L1)']
+    },
+
+    'should parse parent tags wxith tag children (L2)': {
+
+        input: '<panel><a href="link" onclick={{this.someting.invoke()}}>' +
+            'Click Here</a><table/></panel>',
+        expect: expects['should parse parent tags wxith tag children (L2)']
+
+
+    },
+
+    'should parse parent tags wxith tag children (L3)': {
+
+        input: '<panel><a href="link">Click Here</a><table/><panel c="22"></panel></panel>',
+        expect: expects['should parse parent tags wxith tag children (L3)']
+
+    },
+
+    'should do it all together now': {
+
+        input: '<modal name="mymodal" x="1" y="2">' +
+            '<modal-header>My Modal</modal-header>' +
+            '<modal-body>' +
+            'Creativxity is inhibxited by greed and corruption.' +
+            '<vote-button/>' +
+            '<vote-count source={{this}}/> Votes' +
+            '<textarea wml:id="ta" disabled size=32 onchange={{this.setText}}>' +
+            ' Various text' +
+            '</textarea>' +
+            '</modal-body>' +
+            '</modal>',
+        expect: expects['should do it all together now']
+
+    },
+    'should parse for expressions': {
+
+        input: '<root>' +
+            '{% for item in list %}' +
+            '<stem>A Stem</stem>' +
+            '{% endfor %}' +
+            '</root>',
+        expect: expects['should parse for expressions']
+
+    },
+
+    'should parse bind expressions': {
+
+        input: '<div onfocus={{this::doAction}}/>',
+        expect: expects['should parse bind expressions']
+
+    },
+    'should parse ternary expressions': {
+
+        input: '<Html.div id={{this.id}}>{{this.check() ? a : b }}</div>',
+        expect: expects['should parse ternary expressions']
+
+
+
+    },
+    'should parse function literals': {
+
+        input: '<button onclick={{(e)=>this.call(e)}}/>',
+        expect: expects['should parse function literals']
+
+    },
+
+    'should parse includes': {
+
+        input: '<tr>{% for x,i in y %}' +
+            '{% include this.getFrags() [ctx1, ctx2] %}{% include val %}{% endfor %}</tr>',
+        expect: expects['should parse includes']
+
+    },
+    'should parse negative numbers': {
+
+        input: '<tag n={{ ( -0.5 + 3) }} m={{(4 + -2)}} g={{ (10 --5) }}/>',
+        expect: expects['should parse negative numbers']
+
+    },
+
+    'should allow filter chaining': {
+
+        input: '<p>{{ this._::value | f1 | f2(2) | f3(this.value) }}</p>',
+        expect: expects['should allow filter chaining']
+
+
+    }
+
+
+
+
 
 };
 
@@ -72,184 +203,6 @@ describe('Parser', function() {
 
             });
         });
-
-        xit('should parse imports', function() {
-
-            input = `import lib from "path/to/libs";  <tag/>`;
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should parse a self closing tag', function() {
-
-            input = '<simple/>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should parse a self closing tag wxith attributes', function() {
-
-            input = '<user name="xyaa aaz" posxition={{4|x(20)}} wml:val="test"/>';
-            parse();
-            compare(result, expects[this.test.txitle][0]);
-
-            input = '<user enabled id=24 />';
-            parse();
-            compare(result, expects[this.test.txitle][1]);
-
-            input = '<user name="xyaa aaz" id="24" align="left"/>';
-            parse();
-            compare(result, expects[this.test.txitle][2]);
-
-        });
-
-        xit('should parse a parent tag', function() {
-
-            input = '<panel>  \n\n\n\n\n\n\n\n\n  </panel>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should parse a parent tag wxith attributes', function() {
-
-            input = '<panel type="default" size="40" align="left"> </panel>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should parse parent tags wxith mixed children', function() {
-
-            input = '<panel>This is my offsprings.<a>Link</a>Hey now! <Input/></panel>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should parse parent tags wxith tag children (L1)', function() {
-
-            input = '<panel><a></a></panel>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should parse parent tags wxith tag children (L2)', function() {
-
-            input = '<panel><a href="link" onclick={{this.someting.invoke()}}>' +
-                'Click Here</a><table/></panel>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-
-        });
-
-        xit('should parse parent tags wxith tag children (L3)', function() {
-
-            input = '<panel><a href="link">Click Here</a><table/><panel c="22"></panel></panel>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should do xit all together now', function() {
-
-            input = `<modal name="mymodal" x="1" y="2">
-                        <modal-header>My Modal</modal-header>
-                        <modal-body>
-                         Creativxity is inhibxited by greed and corruption.
-                         <vote-button/>
-                         <vote-count source={{this}}/> Votes
-                         <textarea wml:id="ta" disabled size=32 onchange={{this.setText}}>
-                           Various text
-                         </textarea>
-                        </modal-body>
-                    </modal>`;
-
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should parse for expressions', function() {
-
-            input = '<root>' +
-                '{% for xitem in list %}' +
-                '<stem>A Stem</stem>' +
-                '{% endfor %}' +
-                '</root>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-
-        });
-
-        xit('should parse if expressions', function() {
-
-            input =
-                '<div>' +
-                '{% if (length(values()) >= 12) %}' +
-                'Your length is more than 11!' +
-                '<eye>Tiger</eye>' +
-                '{% endif%}' +
-                '</div>';
-
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should parse bind expressions', function() {
-
-            input = '<div onfocus={{this::doAction}}/>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should parse ternary expressions', function() {
-
-            input = '<div id={{this.id}}>{{this.check() ? a : b }}</div>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should parse function lxiterals', function() {
-
-            input = '<button onclick={{(e)=>this.call(e)}}/>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should parse includes', function() {
-
-            input = '<tr>{% for x,i in y %}{% include this.getFrags() [ctx1, ctx2] %}{% include val %}{% endfor %}</tr>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should parse negative numbers', function() {
-
-            input = '<tag n={{ ( -0.5 + 3) }} m={{(4 + -2)}} g={{ (10 --5) }}/>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
-        xit('should allow filter chaining', function() {
-
-            input = '<p>{{ value | f1 | f2(2) | f3(this.value) }}</p>';
-            parse();
-            compare(result, expects[this.test.txitle]);
-
-        });
-
 
     });
 
