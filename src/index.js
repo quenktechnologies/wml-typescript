2a264c9187db7cbb719d9a2f570ac4b14177efbf
Lasana Murray <lmurray@quenk.com>
2017-06-26 10:55:39 -0400

    feature: output can be transpiled directly from compile.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import Parser from './parser/Parser';
 import * as nodes from './parser/nodes';
+import * as babel from 'babel-core';
+import es2015 from 'babel-preset-es2015';
+import exports from 'babel-plugin-transform-export-extensions';
 
 const DEFAULT_OPTIONS = {
   debug: false,
+  es2015: false,
   fileName: 'wmloutput.js'
 };
 
-export function compile(src, ast = nodes, o = DEFAULT_OPTIONS) {
+export function es5ify(js) {
 
-  return Parser.parse(src, nodes).transpile();
+  return babel.transform(js, {
+    sourceMaps: true,
+    presets: [es2015],
+    plugins: [exports],
+    highlightCode: false
+  }).code;
+
+}
+
+export function compile(src, o = DEFAULT_OPTIONS, ast = nodes) {
+
+  let output = Parser.parse(src, nodes).transpile();
+  return (o.es2015) ? es5ify(output) : output;
 
 }
 
